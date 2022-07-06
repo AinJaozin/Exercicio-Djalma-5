618c747c2e797b6b45ecac4587110cbae10b6b9d
55119 <55119@DESKTOP-3E6FVKU>
2022-07-06 19:46:09 -0300

    Adicionar arquivos de projeto.

---
+++ Djalma5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Djalma5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i * 2);
+            }
+            Console.ReadLine();
+
+            //-------------------------------------------------------------------------------------------------------------
+
+            Console.ReadKey();
+        }
+    }
+}
